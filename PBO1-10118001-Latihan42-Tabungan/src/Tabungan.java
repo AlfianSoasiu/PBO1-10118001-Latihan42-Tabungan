10c614bfa067a0a505b865d4f5b00e2d26926120
Alfian Soasiu <55717248+AlfianSoasiu@users.noreply.github.com>
2019-11-07 23:50:01 +0700

    Add files via upload

---
+++ PBO1-10118001-Latihan42-Tabungan/src/Tabungan.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author 
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menghitung tabungan
+ */
+public class Tabungan {
+    
+    private final int saldo;
+    
+    
+    public Tabungan(int saldo) {  
+      this.saldo = saldo;
+    }
+
+    public int ambilUang (int jumlah){
+        return saldo - jumlah;
+    }
+}
---
+++ PBO1-10118001-Latihan42-Tabungan/src/outputtabungan.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author
+ * Nama      : Alfian Soasiu
+ * Kelas     : PBO1
+ * NIM       : 10118001
+ * Deskripsi : Menampilkan output tabungan
+ */
+public class outputtabungan {
+    public static void main(String[] args) {
+        
+        Scanner scn = new Scanner (System.in);
+        
+        System.out.println("_________Program Penarikan Uang_________");
+        System.out.print("Masukkan Saldo Awal : ");
+        Tabungan tabungan = new Tabungan(scn.nextInt());
+        System.out.print("Jumlah uang yang diambil : ");        
+        System.out.print("Saldo anda Sekarang : " 
+                + tabungan.ambilUang(scn.nextInt())+ "\n");
+    }
+}
